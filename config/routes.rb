cb6f80620b47da6923609425c508629c499ba077
micheals-tech <tim@micheals.tech>
2025-08-03 14:15:46 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "pages#home"
+
+  # You can define other routes here as your site grows.
+  # For example, a contact form submission:
+  # post "contact", to: "pages#contact_submit"
+end
